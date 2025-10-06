d69189c892ce9e7c16c8c05def06aa62baa53f4e
masoodc <masoodofficial11@gmail.com>
2025-10-06 14:55:17 +0530

    Add files via upload

---
+++ 19-27 sep 2025  s1/firstchar.py
@@ -0,0 +1,3 @@
+f= str(input("enter the word : "))
+result = f[0]+ f[1:].replace( f[0] ,"$")
+print(result)
---
+++ 19-27 sep 2025  s1/length.py
@@ -0,0 +1,9 @@
+list1=[25,1,3,4,7,30]
+list2=[15,16,39]
+i=len(list1)
+k=len(list2)
+if i==k:
+    print("the length of two list are same")
+else :
+    print("the length of two list are different")
+

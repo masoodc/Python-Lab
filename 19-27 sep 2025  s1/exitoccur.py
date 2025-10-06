d5e56c5522e27fc2bd5173221146f6bc2be46925
masoodc <masoodofficial11@gmail.com>
2025-10-06 16:15:08 +0530

    Update exitoccur.py

--- 19-27 sep 2025  s1/exitoccur.py
+++ 19-27 sep 2025  s1/exitoccur.py
@@ -1,15 +1,28 @@
 z=1
 while(z==1):
-    print("1> occurence of a word")
+    print("\n1> occurence of a word")
     print("2> character frequency of a word")
     print("3> factors of number")
     print("4> Exit")
-    text=str(input("enter a sentence: "))
-    word=text.split() hello are ou
-    s=set(word)
-    for i in s:
-        print(i, "= ",word.count(i))
-    else :
-        print(text,"it is zero")
+    n=int (input("enter choice: "))
+    if n==1:
+        text=str(input("enter a sentence: "))
+        word=text.split()
+        s=set(word)
+        for i in s:
+            print(i, "= ",word.count(i))
+    elif n==2:
+        text=str(input("enter a word: "))
+        s=set(text)
+        for i in s:
+            print(i, "= ",text.count(i))
+    elif n==3:
+        f=int (input("enter number: "))
+        fact=1
+        for i in range(1,f+1):
+            fact*= i
+        print("factorial: ",fact)
+    elif n==4:
+        break;
 else :
      print("while else")
